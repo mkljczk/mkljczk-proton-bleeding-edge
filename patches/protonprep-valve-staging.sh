f1307b41aef0b278d50a1eee63e11e8758ec0a7c
GloriousEggroll <gloriouseggroll@gmail.com>
2025-03-29 11:57:12 -0600

    proton: import upstream changes
    Signed-off-by: Nicole Mikołajczyk <git@mkljczk.pl>

--- patches/protonprep-valve-staging.sh
+++ patches/protonprep-valve-staging.sh
@@ -17,14 +17,7 @@
     pushd vkd3d-proton
     git reset --hard HEAD
     git clean -xdf
-<<<<<<< HEAD
-    
-    #echo "VKD3D-PROTON: -Nvidia Reflex- Add NV low latency support"
-    #pushd khronos/Vulkan-Headers; git pull; git checkout bbe0f575ebd6098369f0ac6c6a43532732ed0ba6; popd
-    #patch -Np1 < ../patches/proton/81-nv_low_latency_vkd3d_proton.patch
-=======
 
->>>>>>> 00724612 (patches: add new patches to protonprep)
     popd
 
     pushd dxvk-nvapi
